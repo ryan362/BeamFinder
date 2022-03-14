346528366ecd26dfc5afec43cf7aa31238340744
Ryxn1 <59591086+ryan362@users.noreply.github.com>
2022-03-14 15:34:22 +0000

    fixed formula seems to work

--- index.js
+++ index.js
@@ -3,35 +3,37 @@ const prompt = require('prompt-sync')();
 var dead = parseFloat(prompt('Dead Load: ', 60))
 var live = parseFloat(prompt('Live Load: ', 100))
 var length = parseFloat(prompt('Beam Length: ', 20))
-var spacing = parseFloat(prompt('Beam Spacing: ', 8))
+var spacing = parseFloat(prompt('Beam Spacing: '))
 
 const liveTot = live * spacing
 const weight = (dead * spacing) + liveTot
 const Ra = (weight * length)/2
 //console.log('Ra/Rb = ' + Ra +'lbs')
-const Mmax = (weight * (length**2))/spacing
+const Mmax = (weight * (length**2))/8
 const Mn = 1.67*Mmax
+console.log(Mn)
 const Zx = (Mn * 12)/50000
 console.log('Zx = ' + Zx + " in^3")
 console.log('Please choose a beam')
 
-var depth = parseFloat(prompt('Beam Depth: ', 13.74))
-var thickness = parseFloat(prompt('Web Thickness: ', 0.23))
-var Ix = parseFloat(prompt('Ix: ', 199))
+var depth = parseFloat(prompt('Beam Depth: '))
+var thickness = parseFloat(prompt('Web Thickness: '))
+var Ix = parseFloat(prompt('Ix: '))
 const Vn = 0.6 * 50000 * depth * thickness
 
 if (Vn > (1.5 * Ra)) {
 	console.log("Passed shear strength")
 }else{
-	console.log("Does not pass shear strength test, please refresh and pick a new beam")
+	console.log("Does not pass shear strength test, please pick a new beam")
 	exit();
 }
 
 //Deflection
 const bothLim = (length * 12)/240
 const liveLim = (length * 12)/360
+console.log(liveLim)
 
-const maxWeight = (5 * weight * (length**4)*12)/(384 * 29000000 * Ix)
+const maxWeight = (5 * weight * (length**4)*12**3)/(384 * 29000000 * Ix)
 if (maxWeight < bothLim) {
 	console.log("Passed Dead + Live limit")
 }else{
@@ -39,20 +41,20 @@ if (maxWeight < bothLim) {
 	exit();
 }
 
-const liveMaxWeight = (5 * liveTot * (length**4))/(384 * 29000000 * Ix)
-if (maxWeight < liveLim) {
+const liveMaxWeight = (5 * liveTot * (length**4)*12**3)/(384 * 29000000 * Ix)
+if (liveMaxWeight < liveLim) {
 	console.log("Passed Live limit")
 }else{
-	console.log('Doesn\'t pass the live limits, try another beam')
+	console.log(`Doesn\'t pass the live limits, try another beam ${liveMaxWeight}`)
 	exit();
 }
-console.log('Beam Passed all checks, dispensing values')
-console.log(`All Values below: \n
-Total weight = ${weight} plf\n
-Ra = ${Ra} lb\n
-Mmax = ${Mmax} ftlb\n
-Mn = ${Mn} ftlb\n
-Zx = ${Zx} in^3\n
+console.log('Beam Passed all checks, printing values')
+console.log(`\nAll Values below: \n
+Total weight = ${weight} plf
+Ra = ${Ra} lb
+Mmax = ${Mmax} ftlb
+Mn = ${Mn} ftlb
+Zx = ${Zx} in^3
 Vn = ${Vn} lb\n
 Deflection Limits:
 Dead+Live = ${bothLim} in
